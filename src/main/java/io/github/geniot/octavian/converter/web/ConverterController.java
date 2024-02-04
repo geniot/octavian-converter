ab4f8fd1eb5ee6d7f391f0dee0ae7db9fe0fa3ef
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2024-02-04 15:09:41 +0100

    Try to see response that depends on the input

--- src/main/java/io/github/geniot/octavian/converter/web/ConverterController.java
+++ src/main/java/io/github/geniot/octavian/converter/web/ConverterController.java
@@ -7,12 +7,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+
 @RestController
 public class ConverterController {
     @PostMapping(value = "/convert", consumes = {"application/json"})
     public @ResponseBody
     MuseConversionResponse handleRefreshSubscription(@RequestBody MuseConversionRequest request) {
         MuseConversionResponse result = new MuseConversionResponse();
+
+        IntBuffer intBuf =
+                ByteBuffer.wrap(request.getAuthor().getBytes())
+                        .order(ByteOrder.BIG_ENDIAN)
+                        .asIntBuffer();
+        int[] array = new int[intBuf.remaining()];
+        intBuf.get(array);
+
+        result.setRepeats(array);
         return result;
     }
 }
